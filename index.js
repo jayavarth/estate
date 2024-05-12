1b74407b4e0ddbb50f4a492b2c944ebf92747ab9
jayavarth <123160124+jayavarth@users.noreply.github.com>
2024-05-12 14:53:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -64,19 +64,18 @@ app.post('/login', async (req, res) => {
   }
 });
 
-// Middleware to verify JWT token
 const verifyToken = (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) {
-    return res.status(401).json({ error: 'Unauthorized' });
+      return res.status(401).json({ error: 'Unauthorized' });
   }
 
   jwt.verify(token, 'secret', (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-    req.userId = decoded.userId;
-    next();
+      if (err) {
+          return res.status(401).json({ error: 'Unauthorized' });
+      }
+      req.userId = decoded.userId;
+      next();
   });
 };
 
